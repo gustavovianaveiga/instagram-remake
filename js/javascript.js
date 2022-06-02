39303d9c86640fa538c8f6ee3630730da254c019
gustavo viana <gustavovianaveiga721@gmail.com>
2022-06-01 22:22:55 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,82 @@
+function setadois() {
+    var julia = document.getElementById('julia');
+    var pablo = document.getElementById('pablo');
+    var luis = document.getElementById('luis');
+    var joao = document.getElementById('joao');
+    var pedro = document.getElementById('pedro');
+    var alberto = document.getElementById('alberto');
+    var ana = document.getElementById('ana');
+    var gabriel = document.getElementById('gabriel');
+    var fabio = document.getElementById('fabio');
+    var francisco = document.getElementById('francisco')
+    var maria = document.getElementById('maria');
+
+    julia.src = ('http://pm1.narvii.com/8040/082d8d7e8b1c814cc1dc684b9567da70d0ba0723r1-256-256v2_uhq.jpg');
+    pedro.src = ('https://pbs.twimg.com/profile_images/707960588505370625/K2Aaqcqp_400x400.jpg');
+    luis.src = ('https://conteudo.imguol.com.br/c/entretenimento/7f/2022/02/07/bbb-22-arthur-aguiar-em-noite-de-jogo-da-discordia-1644285396028_v2_1x1.png');
+    pablo.src = ('https://moradadamemoria-files.s3.us-east-2.amazonaws.com/372772456_610051185688756607_501446747_639796628291135662.jpg');
+    joao.src = ('https://moradadamemoria-files.s3.us-east-2.amazonaws.com/576879341_540262523754239678_733424354_265078170634868315.jpg');
+    alberto.src = ('https://sapl.cabrobo.pe.leg.br/media/sapl/public/parlamentar/8/joao_pedro_torres_cavalcante_novaes_-_vice-presidente.jpg.256x256_q85_box-0%2C19%2C424%2C443_crop_detail.jpg');
+    ana.src = ('https://media-exp1.licdn.com/dms/image/C5603AQF2_BrbzAGxMA/profile-displayphoto-shrink_200_200/0/1517037987268?e=1652918400&v=beta&t=JnGQBh85YzrzUrTB9Kz0sC5UeCknqF7Ssm_oxp-dYUI');
+    gabriel.src = ('https://images.pexels.com/users/avatars/3691654/joao-victor-valeriote-252.jpeg?auto=compress&fit=crop&h=256&w=256');
+    fabio.src = ('https://images.pexels.com/users/avatars/66654148/joao-almeida-579.jpeg?auto=compress&fit=crop&h=256&w=256');
+    francisco.src = ('https://www.netflu.com.br/wp-content/uploads/2019/09/pedro-1.png');
+    maria.src = ('https://yt3.ggpht.com/ytc/AKedOLQiI6bmbWs560NgiONauh0vCM7JO20H3IsSmI0S-g=s900-c-k-c0x00ffffff-no-rj');
+
+    const nomes = ['julho', 'cesar', 'gustavo', 'fabiana', 'clara', 'fernanda', 'augusto', 'marcos', 'antonio', 'carlos', 'andre', 'faustão', 'marcio']
+    juliaa.innerHTML = nomes[3];
+    anaa.innerHTML = nomes[4];
+    mariaa.innerHTML = nomes[5];
+    fabioo.innerHTML = nomes[0];
+    gabriell.innerHTML = nomes[1];
+    albertoo.innerHTML = nomes[2];
+    pedroo.innerHTML = nomes[6];
+    franciscoo.innerHTML = nomes[7];
+    pabloo.innerHTML = nomes[8];
+    joaoo.innerHTML = nomes[9];
+    luiss.innerHTML = nomes[10];
+
+
+};
+
+function setaum() {
+    var julia = document.getElementById('julia');
+    var pablo = document.getElementById('pablo');
+    var luis = document.getElementById('luis');
+    var joao = document.getElementById('joao');
+    var pedro = document.getElementById('pedro');
+    var alberto = document.getElementById('alberto');
+    var ana = document.getElementById('ana');
+    var gabriel = document.getElementById('gabriel');
+    var fabio = document.getElementById('fabio');
+    var francisco = document.getElementById('francisco')
+    var maria = document.getElementById('maria');
+
+    julia.src = ('https://cdn.domestika.org/c_fill,dpr_auto,f_auto,h_256,pg_1,t_base_params,w_256/v1588704894/avatars/002/741/699/2741699-original.JPG?1588704894');
+    pedro.src = ('https://tntsports.com.br/__export/1579893452487/sites/esporteinterativo/img/2020/01/24/pedro_1_crop1579892206794.jpg_1270962302.jpg');
+    luis.src = ('https://www.camara.leg.br/internet/deputado/bandep/pagina_do_deputado/204381.jpg');
+    pablo.src = ('https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/d/8/7/2/d872639bee77b8e4d767f6922ae1850c.jpg');
+    joao.src = ('https://yt3.ggpht.com/ytc/AKedOLT2ZsYEQqIdLrX2x53c_jYRcbbkxS-XBemBNPXRCw=s900-c-k-c0x00ffffff-no-rj');
+    alberto.src = ('https://www.tupi.fm/wp-content/uploads/valentim-cuiaba.jpg');
+    ana.src = ('https://p2.trrsf.com/image/fget/cf/1200/1200/filters:quality(85)/images.terra.com/2022/01/22/1798086607-ana-de-armas.jpg');
+    gabriel.src = ('https://conteudo.imguol.com.br/c/esporte/e7/2022/02/10/gabriel-volante-do-inter-em-anuncio-oficial-pelo-clube-1644524897370_v2_1x1.jpg');
+    fabio.src = ('https://images.pexels.com/users/avatars/66654148/joao-almeida-579.jpeg?auto=compress&fit=crop&h=256&w=256');
+    francisco.src = ('https://www.thefamouspeople.com/profiles/images/francisco-lachowski-1.jpg');
+    maria.src = ('https://styles.redditmedia.com/t5_50igsm/styles/communityIcon_fzi3krcivpn71.jpg?width=256&s=3872827b3df29a0aed7826a2881f557dae6a001c');
+
+    juliaa.innerHTML = 'julia';
+    anaa.innerHTML = 'ana';
+    mariaa.innerHTML = 'maria';
+    fabioo.innerHTML = 'fabio';
+    gabriell.innerHTML = 'gabriel';
+    albertoo.innerHTML = 'alberto';
+    pedroo.innerHTML = 'pedro';
+    franciscoo.innerHTML = 'francisco';
+    pabloo.innerHTML = 'pablo';
+    joaoo.innerHTML = 'joão';
+    luiss.innerHTML = 'luis';
+};
+
+
+
+$('.postimg').slick();
